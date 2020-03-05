ca90a2432cd56b29fcef467ea6d726e1871172c0
ZhuHai <40315258+freenowill@users.noreply.github.com>
2020-03-05 11:09:02 +0800

    Add files via upload

--- add_noise.py
+++ add_noise.py
@@ -2,7 +2,10 @@ import numpy as np
 import librosa
 import os
 from tqdm import tqdm
+import random
 def add_noise(x, d, SNR):
+    assert(len(d)>len(x))
+    d=d[:len(x)]
     P_signal=np.sum(abs(x)**2)
     P_d=np.sum(abs(d)**2)
     P_noise=P_signal/10**(SNR/10)
@@ -16,19 +19,28 @@ def wgn(x, snr):
     P_noise = P_signal/10**(snr/10.0)
     return np.random.randn(len(x)) * np.sqrt(P_noise)
 
-def preprocess(path,id,x,d=None,SNR=5):
-    if d==None:
-        d=wgn(x,SNR)
+def preprocess(x,d,SNR=5):
     data=add_noise(x,d,SNR)
     data=np.float32(data)
     return data
 
 if __name__ == '__main__':
     from utils.util import load_audio,save_audio
-    path="D:/ftp/hey_snips_kws_4.0/hey_snips/dev/dev_positive_vad/"
-    out_path="D:/ftp/hey_snips_kws_4.0/hey_snips/dev/dev_positive_vad_SNR(5db)/"
+    path="D:/ftp/hey_snips_kws_4.0/hey_snips/train/train_positive_vad/"
+    random_noise="D:/ftp/hey_snips_kws_4.0/random/"
+    out_path="D:/ftp/hey_snips_kws_4.0/hey_snips/train/train_positive_vad(random_noise_SNR_5db)/"
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
     ids=os.listdir(path)
+    noises=os.listdir(random_noise)
+
     for id in tqdm(ids):
         x = load_audio(path+id)
-        data=preprocess(path,id,x,SNR=5)
-        save_audio(out_path+id,data)
+
+        noise = noises[random.randint(0, len(noises) - 1)]
+        d = load_audio(random_noise+noise)
+        while len(d)<len(x):
+            noise = noises[random.randint(0, len(noises) - 1)]
+            d = load_audio(random_noise + noise)
+        data=preprocess(x,d,SNR=5)
+        save_audio(out_path+"-snr5db-"+noise+"-"+id,data)
